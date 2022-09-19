6d196be3809a75abbf471c517a2bd01836e65f56
Piyush Pipriye <pipriyepiyush00@gmail.com>
2022-09-19 21:38:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// const app = require('./app')
+// console.log("sub")
+// var a = 10
+// var b= 20
+// console.log(a,b)
+// a= "123"
+// b= 123
+// if(a===b){
+//     console.log("got it")
+// }
+
+// console.log(app.x+app.y)
+// console.log(app.z())
+
+
+// arr=[1,2,3,4,5,6]
+// let ar = arr.filter((items)=>{
+// return items>1
+// })
+// console.log(ar)
+// const fs = require('fs');
+//  fs.writeFileSync("demo.txt","hellllllllo alll creating a file")
+
+
+// const http = require('http')
+// http.createServer((req, res) => {                                    // create a basic server
+//     res.write("<h1>hello, this is piyush pipriye")
+//     res.end()
+// }).listen(4500)
