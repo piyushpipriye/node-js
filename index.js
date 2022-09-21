e92d48c0bd24d8decf5bb80c5feca5e50fe6dd2c
Piyush Pipriye <pipriyepiyush00@gmail.com>
2022-09-21 21:15:30 +0530

    index file changes

--- index.js
+++ index.js
@@ -32,3 +32,31 @@
 //var colors = require('colors');
 //console.log("hello".green,"all".red)        // assign color to text
 
+//console.log("aeeeeeeeeeee lavde 80 hazar ke shoes hai tera ghr jayega isme smja kta tera ghr isme chla jayega")
+
+// const data = require('./api_data')
+// const http = require('http')
+// http.createServer((req, res) => {                                    // create a basic server
+//     res.writeHead(450,{'Content-Type':'application\json'})          // defining status code where 450  is staus code
+//     res.write(JSON.stringify(data))                                 // printing static data from another file by api 
+//     res.end()
+// }).listen(400)
+
+   // console.log(process.argv[2])   // argv stands for argument vector which will help to acces the i/p given by the user[index_no]
+                            // 0 & 1 are reserved index nos afters words it start like node .\index.js any_input_provide_here
+
+
+                            
+// const fs= require('fs')             // import file module/package 
+// const input = process.argv          // accessing or taking input from user in the form of array
+// if(input[2]=='add'){
+// fs.writeFileSync(input[3],input[4])        //giving file name and its content [2] will provide file name & [3] will file content
+// console.log("File added successfully")
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+//     console.log("File deleted successfully")           // this will delete the file for that we used fs.unlinksync function
+// }
+// else{
+//     console.log("invalid choice")
+// }\
