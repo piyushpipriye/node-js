f6a9d5146a13c694cd24e61801da4fa3cc352522
Piyush Pipriye <pipriyepiyush00@gmail.com>
2022-10-26 23:11:19 +0530

    2nd commit

--- index.js
+++ index.js
@@ -252,9 +252,8 @@
 // app.listen(5000)
 
 const mongoose = require('mongoose');
-
+mongoose.connect("mongodb://localhost:27017/e-commerce")
 const insert = async () => {
-    const res = await mongoose.connect("mongodb://localhost:27017/e-commerce")
     const productSchema = new mongoose.Schema({
         name: String,
         brand: String,
@@ -268,4 +267,20 @@ const insert = async () => {
     console.log(result)
 }
 
-insert()
+//insert()
+
+const update = async () => {
+    const productSchema = new mongoose.Schema({
+        name: String,
+        brand: String,
+        price: Number,
+        category: String
+    });
+
+    const productModel = new mongoose.model('products', productSchema);
+    let data = new productModel({ name: "6 s", brand: "iphone", price: 25000, category: "mobile" })      // can add only those fields which are defined by schemas only
+    let result = await data.save();
+    console.log(result)
+}
+
+//update()
