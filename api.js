2785e836a3e6e9cbcc986ebc8306c69223b0ef83
Piyush Pipriye <pipriyepiyush00@gmail.com>
2022-10-26 17:45:00 +0530

    first commit

--- api.js
+++ api.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const dbConnectoin = require('./mongodb')
-const main = require('./readData')
+const fetch = require('./readData')
 const insert = require('./insertData')
+const update = require('./updateData')
+const deleteData = require('./deleteData')
 
 app.use(express.json());
 
 
 app.get('/', async (req, res) => {
-    data = await main()
+    data = await fetch()
     // let data  = await dbConnectoin()        // 2nd way of handline the promises
     //  data = await data.find().toArray();     // can fetch specific data also by giving arguments in find() method 
     // console.log(data)
@@ -23,5 +25,15 @@ app.post('/', async (req, resp) => {
     // let result = await data.insert(req.body)        // inserting data through postman
     resp.send(data)
 })
+
+
+app.post('/', async (req, resp) => {
+    data = await update(req.body[0],req.body[1])
+    // console.log(req.body)
+    // let data = await dbConnectoin()
+    // let result = await data.insert(req.body)        // inserting data through postman
+    resp.send(data)
+})
+
 
 app.listen(5000)
