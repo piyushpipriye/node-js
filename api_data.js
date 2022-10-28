39f29dc9a98b7142c494fc4c86aade2f53679b5b
piyushpipriye <pipriyepiyush00@gmail.com>
2022-10-28 16:24:33 +0530

    delete

--- api_data.js
+++
@@ -1,8 +0,0 @@
-const data=[
-    {name:'Ashutosh Pipriye',email:'ashutoshpipriye24@gmail.com',mobile_no:'7972854011'},
-    {name:'Piyush Pipriye',email:'piyushpipriye12@gmail.com',mobile_no:'9763131436'},
-    {name:'Prathmesh Makariye',email:'prathmeshmakariye@gmail.com'},
-    {name:'Varun Pipriye',email:'piyushpipriye12@gmail.com'},
-    {name:'Omkar Mahatole',email:'omkarmahatole@gmail.com'}
-]
-module.exports=data
