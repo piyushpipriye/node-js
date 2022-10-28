c8c33d6d7a4befaeafa532abfc1e7f74c1224cde
piyushpipriye <pipriyepiyush00@gmail.com>
2022-10-28 22:42:49 +0530

    1st commit

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const os = require('os');
-console.log(os.arch())
-console.log(os.freemem() / (1024 * 1024 * 1024))
-console.log(os.totalmem() / (1024 * 1024 * 1024))
-console.log(os.hostname())
-console.log(os.platform())
-console.log(os.userInfo())
+// event for how many times api called 
+
+const express = require('express');
+const EventEmitter = require('events')
+const app = express();
+
+const event = new EventEmitter();       // creating event object
+
+let count = 0
+event.on("countApi", () => {        // event handling
+    count++;
+    console.log("event called", count)
+})
+
+
+app.get('/', (req, resp) => {
+    resp.send("Called")
+    event.emit("countApi")          // event calling
+});
+
+app.get('/search', (req, resp) => {
+    resp.send("search Called")
+    event.emit("countApi")
+});
+
+app.get('/update', (req, resp) => {
+    resp.send("update Called")
+    event.emit("countApi")
+});
+
+app.listen(5000)
