30915e5af524078ae57e86ecf27d10510ae84d25
piyushpipriye <pipriyepiyush00@gmail.com>
2022-10-28 15:35:54 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,81 +1,35 @@
-// const mongoose = require('mongoose');
-// mongoose.connect("mongodb://localhost:27017/e-commerce")
+const express = require('express')
+const path = require('path')
 
-// const productSchema = new mongoose.Schema({     // Defining schemas
-//     name: String,
-//     brand: String,
-//     price: Number,
-//     category: String
-// });
+const app = express()
+const pubpath = path.join(__dirname, 'public')
+console.log(pubpath)
+app.use(express.static(pubpath))     // use method does not provide route functionality 
 
-// const productModel = mongoose.model('products', productSchema);     // Defining model
+app.set('view engine', 'ejs')
 
-// const insert = async () => {
-//     let data = new productModel({ name: "6 s", brand: "iphone", price: 25000, category: "mobile" })      // can add only those fields which are defined by schemas only
-//     let result = await data.save();
-//     console.log(result)
-// }
-// //insert()
-
-// const update = async () => {
-//     let result = await productModel.updateOne(
-//         { name: "6 s" },
-//         {
-//             $set: { price: 20000, name: "7 s" }
-//         }
-//     )
-//     console.log(result)
-// }
-// //update()
-
-// const dele = async () => {
-//     let result = await productModel.deleteOne(
-//         { name: "7 s" }
-//     )
-//     console.log(result)
-// }
-// //dele()
-
-// const fetch = async () => {
-//     let result = await productModel.find()
-//     console.log(result)
-// }
-// // fetch()
-
-
-const express = require('express');
-require('./config')
-const products = require('./product')
-
-const app = express();
-app.use(express.json())
-
-app.post('/create', async (req, resp) => {
-    let data = new products(req.body)
-    let result = await data.save()
-    resp.send(result);
+app.get('/', (req, res) => {
+    res.sendFile(`${pubpath}/index.html`)
+})
+app.get('/profile', (req, res) => {
+    const user = {
+        name: 'piyush pipriye',
+        email: 'pipriyepiyush00@gmail.com',
+        city: 'aurangabad',
+        skills: ['php', 'node js', 'javascript']
+    }
+    res.render('profile', { user })
 })
 
-app.get('/get', async (req, resp) => {
-    let data = await products.find()
-    resp.send(data)
+app.get('/about', (req, res) => {
+    res.sendFile(`${pubpath}/about.html`)
 })
 
-app.delete('/delete', async (req, resp) => {
-    let res = await products.deleteOne(
-        req.body
-    )
-    resp.send(res)
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.get('*', (req, res) => {
+    res.sendFile(`${pubpath}/pagenotfound.html`)
 })
 
-app.put('/update', async (req, resp) => {
-    let result = await products.update(
-        { name: req.body.name },
-        {
-            $set: req.body
-        }
-    )
-    resp.send(result)
-})
-
-app.listen(5000);
+app.listen(5000)
