52ca0bb1bc2671781b6ad8e85544ba22134e96a7
piyushpipriye <pipriyepiyush00@gmail.com>
2022-10-28 16:21:47 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const data = require('./api_data')
-const http = require('http')
-http.createServer((req, res) => {                                    // create a basic server
-    res.writeHead(450, { 'Content-Type': 'application\json' })          // defining status code where 450  is staus code
-    res.write(JSON.stringify(data))                                 // printing static data from another file by api 
-    res.end()
-}).listen(400)
+const express = require('express')
+const app = express()
+const reqFilter = require('./middleware')
+const route = express.Router();
+
+route.use(reqFilter);
+// app.use(reqFilter)                           // used for application level route
+
+app.get('/', (req, res) => {
+    res.send("Welcome to home page")
+})
+
+app.get('/users', reqFilter, (req, res) => {    //applies middleware to specific that route only    // apply middleware to single route
+    res.send("Welcome to users page")
+})
+
+route.get('/about', (req, res) => {                 //group level route to apply middleware
+    res.send("Welcome to about page")
+})
+
+route.get('/contact', (req, res) => {
+    res.send("Welcome to contact page")         //group level route to apply middleware
+})
+
+app.use('/', route);
+
+app.listen(5000)
